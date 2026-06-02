6b06fb6f7e54a031fe750b8dcab49f04d5e20f9a
Zanehisako <yassinetakiko@gmail.com>
2026-06-02 09:29:29 +0100

    Revert "first commit"
    This reverts commit 3d0c23dc402b72ac245a1b37ed502a420d3c4f10.

--- build/CMakeFiles/fc-tmp/cpp_httplib/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for cpp_httplib")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/mac/Projects/agent_Engine/build/CMakeFiles/fc-tmp/cpp_httplib/cpp_httplib-gitclone.cmake")
-
-endblock()
--- build/dev/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mac/Projects/agent_Engine/tests
-# Build directory: /Users/mac/Projects/agent_Engine/build/dev/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[AgentEngineTests]=] "/Users/mac/Projects/agent_Engine/build/dev/bin/AgentEngineTests")
-set_tests_properties([=[AgentEngineTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/mac/Projects/agent_Engine/tests/CMakeLists.txt;19;add_test;/Users/mac/Projects/agent_Engine/tests/CMakeLists.txt;0;")
