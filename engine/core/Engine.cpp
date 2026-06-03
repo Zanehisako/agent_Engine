56e90940fe05a51c9902fc04977d1342977f9e35
Zanehisako <yassinetakiko@gmail.com>
2026-06-03 12:16:39 +0100

    Better Poll event logging

--- engine/core/Engine.cpp
+++ engine/core/Engine.cpp
@@ -1,4 +1,29 @@
 #include "AgentEngine/Engine.hpp"
+#include <print>
+
+#define SDL_EVENT_LIST(X)                    \
+    X(SDL_EVENT_QUIT)                        \
+    X(SDL_EVENT_KEY_DOWN)                    \
+    X(SDL_EVENT_KEY_UP)                      \
+    X(SDL_EVENT_MOUSE_BUTTON_DOWN)           \
+    X(SDL_EVENT_MOUSE_BUTTON_UP)             \
+    X(SDL_EVENT_MOUSE_MOTION)                \
+    X(SDL_EVENT_MOUSE_WHEEL)                 \
+    X(SDL_EVENT_WINDOW_RESIZED)              \
+    X(SDL_EVENT_WINDOW_CLOSE_REQUESTED)
+
+const char* GetEventName(Uint32 type)
+{
+    switch (type)
+    {
+#define X(event) case event: return #event;
+        SDL_EVENT_LIST(X)
+#undef X
+
+        default:
+            return "UNKNOWN_EVENT";
+    }
+}
 
 namespace ae
 {
@@ -8,15 +33,16 @@ namespace ae
     }
     void Engine::Update() {
         bool IsRunning = true;
-         SDL_Event Event;
-         while (IsRunning) {
-           while (SDL_PollEvent(&Event)) {
-             if (Event.type == SDL_EVENT_QUIT) {
-               IsRunning = false;
-             }
-             timer.update();
-           }
-         }
+        SDL_Event Event;
+        while (IsRunning) {
+            while (SDL_PollEvent(&Event)) {
+                // std::println("event polled {}",GetEventName(Event.type));
+                if (Event.type == SDL_EVENT_QUIT) {
+                    IsRunning = false;
+                }
+                timer.update();
+            }
+        }
     }
     void Engine::Shutdown() {
         SDL_Quit();
